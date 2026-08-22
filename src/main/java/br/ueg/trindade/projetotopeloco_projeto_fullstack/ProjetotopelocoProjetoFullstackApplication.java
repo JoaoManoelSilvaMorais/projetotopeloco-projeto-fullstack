136b4705e2ef1d1dd5a274a13a7064900574c105
JoaoManoelSilvaMorais <joaomanoelsilvamorais@gmail.com>
2026-08-21 21:13:00 -0300

    feat: initialize fullstack project with React and Spring Boot
    - Added React frontend with Vite, including main entry point and styles.
    - Included SVG assets for React and Vite logos.
    - Set up Spring Boot backend with main application class and application properties.
    - Created test class for application context loading.

---
+++ src/main/java/br/ueg/trindade/projetotopeloco_projeto_fullstack/ProjetotopelocoProjetoFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.projetotopeloco_projeto_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetotopelocoProjetoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotopelocoProjetoFullstackApplication.class, args);
+	}
+
+}
